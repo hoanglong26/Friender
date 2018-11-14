94a4e4982df5cd6821122341aea572e6559ad381
LongPham <long.phamlp94@gmail.com>
2018-11-15 01:51:20 +0700

    add

--- Chinder/app/src/main/java/com/amaris/hoanglong/chinder/MainActivity.java
+++ Chinder/app/src/main/java/com/amaris/hoanglong/chinder/MainActivity.java
@@ -1,27 +1,20 @@
 package com.amaris.hoanglong.chinder;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.amaris.hoanglong.chinder.POJO.Image;
 import com.amaris.hoanglong.chinder.POJO.TinderDirectionalCard;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
@@ -41,9 +34,6 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -240,5 +230,9 @@ public class MainActivity extends AppCompatActivity {
         mSwipeView.doSwipe(true);
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.origin, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
